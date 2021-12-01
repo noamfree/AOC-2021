0a3648534cab28a1715bdde5232ca05ca067e94a
nfreeman <nfreeman@lightricks.com>
2021-12-01 22:10:17 +0200

    day1 part2

--- src/main/kotlin/day1.kt
+++ src/main/kotlin/day1.kt
@@ -1,4 +1,3 @@
-
 private fun tests() {
     val measures = """
         199
@@ -14,22 +13,30 @@ private fun tests() {
     """.trimIndent()
     val numbers = measures.lines().map { it.toInt() }
     assertEquals(numberOfIncreases(numbers), 7)
+    assertEquals(numberOfMaskedIncreasings(numbers, 3), 5)
 }
 
-fun main() {
+private fun main() {
     tests()
     val input = readInputFile("input1")
     println(part1(input))
+    println(part2(input))
 
     // sanity
     println("day1")
 }
 
-private fun parseInput(input: String) = input.lines().map { it.toInt() }
+private fun part2(input: String): Int =
+    numberOfMaskedIncreasings(parseInput(input), 3)
+
+private fun numberOfMaskedIncreasings(numbers: List<Int>, maskSize: Int): Int {
+    return numbers.windowed(maskSize).map { it.sum() }.zipWithNext().count { (a, b) -> b > a }
+}
 
 private fun part1(input: String): Int =
     numberOfIncreases(parseInput(input))
 
-fun numberOfIncreases(numbers: List<Int>): Int {
-    return numbers.zipWithNext().count { (a, b) -> b > a }
-}
+private fun parseInput(input: String) = input.lines().map { it.toInt() }
+
+private fun numberOfIncreases(numbers: List<Int>): Int =
+    numberOfMaskedIncreasings(numbers, 1)
